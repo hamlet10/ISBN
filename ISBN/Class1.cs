29d4a823670997764b6e2de5efdd72c036978651
hamlet10 <hpsanchez@live.com>
2021-09-17 23:18:24 -0400

    two passing test of three

---
+++ ISBN/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ISBN
+{
+    public class ISBN
+    {
+        public static bool ValidISBN10(string input)
+        {
+            return false;
+        }
+
+
+        public static void Input_Validator(string input)
+        {
+
+
+            if (!string.IsNullOrEmpty(input))
+            {
+
+                try
+                {
+                    foreach (var item in input)
+                    {
+                        if (!char.IsDigit(item)) throw new ArgumentException();
+                    }
+                }
+                catch (System.Exception)
+                {
+
+                    throw new ArgumentException();
+                }
+
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+
+        }
+    }
+}
